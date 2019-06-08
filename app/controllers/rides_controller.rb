c131e32095a7b35ad22783cf411adba4f4c5912e
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-08 00:13:34 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -34,6 +34,7 @@ class RidesController < ApplicationController
 
   def tickets_check(attraction)
     if current_user.tickets < attraction.tickets
+      binding.pry
       flash[:alert] ||= []
       flash[:alert] << "You do not have enough tickets to ride the #{attraction.name}."
     end
